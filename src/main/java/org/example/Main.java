9eef088048368010d707e0ddef21a8decb416925
Kukushaa <rexoman65@gmail.com>
2024-07-02 19:07:00 +0400

    task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        int finalAns = countNumber(50, 0);
+    }
 
+    public static int countNumber(int size, int index) {
+        if (size > index) {
+            int allSum = 0;
+            for (index = 0; index < size; index++) {
+                if (index % 2 == 0) {
+                    allSum++;
+                }
+            }
+            return allSum;
+        } else {
+            return -1;
+        }
     }
 }
